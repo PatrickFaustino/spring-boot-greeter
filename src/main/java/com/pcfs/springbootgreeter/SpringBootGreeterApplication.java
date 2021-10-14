b4dfaab290cb9ff702e508f7b0e98020dba05b55
pcfs <patrickc.faustino@gmail.com>
2021-10-14 17:06:19 -0300

    Removing Application test

--- src/main/java/com/pcfs/springbootgreeter/SpringBootGreeterApplication.java
+++ src/main/java/com/pcfs/springbootgreeter/SpringBootGreeterApplication.java
@@ -1,25 +1,13 @@
 package com.pcfs.springbootgreeter;
 
-import com.pcfs.springbootgreeter.library.Greeter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootGreeterApplication implements CommandLineRunner {
-
-	@Autowired
-	private Greeter greeter;
+public class SpringBootGreeterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGreeterApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		String message = greeter.greet();
-		System.out.println(message);
 	}
 
 }
